4b37a674349f000621f4c991925aed8ee73728a7
yiwang <yiwang3@andrew.cmu.edu>
2019-12-01 11:13:19 -0800

    fix video link

--- client/js/mockData/productVideo.js
+++ client/js/mockData/productVideo.js
@@ -25,5 +25,14 @@ module.exports = [
         liveVideoURI: "https://www.youtube.com/watch?v=yFZLkzU8Hw4",
         liveVideoImg: "https://i.loli.net/2019/12/01/ev8xRijZPTIdMso.png",
         id: 10002
+    },
+    {
+        name: "Beecher’s Hope Ranch",
+        location: "4 Parker Rd, Texas, US",
+        videoURI: "https://www.youtube.com/watch?v=yFZLkzU8Hw4",
+        videoImg: "https://i.loli.net/2019/12/01/i1AK4jY2BRLGtPC.png",
+        liveVideoURI: "https://www.youtube.com/watch?v=yFZLkzU8Hw4",
+        liveVideoImg: "https://i.loli.net/2019/12/01/ev8xRijZPTIdMso.png",
+        id: 10003
     }
 ];
