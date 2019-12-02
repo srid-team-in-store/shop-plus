89abe06a1cdbe14485c2e1ec31d5c80ded3b2edb
yiwang <yiwang3@andrew.cmu.edu>
2019-12-02 13:04:43 -0800

    update

--- client/js/mockData/cartItemList.js
+++ client/js/mockData/cartItemList.js
@@ -3,13 +3,13 @@ module.exports = [{
     name: "fairlife Chocolate, 2% reduced fat milk",
     score: 4.9,
     rateImg: "https://i.imgur.com/28VnoAH.png",
-    price: 4.99,
+    price: "$4.99",
     id: 1,
 }, {
     imgSrc: "https://i.imgur.com/t66130U.png",
     name: "Kirkland Signature Organic Blueberries",
     score: 4.7,
     rateImg: "https://i.imgur.com/t57LkuA.png",
-    price: 14.99,
+    price: "$14.99",
     id: 2,
 }];
